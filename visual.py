99466faf349e033216ff8fb8c0e5f5f1f11dfbf1
Sproinkerino <nwfxyz@gmail.com>
2021-06-28 15:09:36 +0800

    V2

--- visual.py
+++ visual.py
@@ -5,7 +5,10 @@ from yahoo_scrap import YahooInfoGet
 import os
 import datetime
 import texttable
+from color_config import *
+
 #
+# #
 # class Test:
 #     def __init__(self):
 #         self.columns = 90
@@ -18,6 +21,7 @@ def clear_console():
         command = 'cls'
     os.system(command)
 
+
 def mystrlen(s):
     return max(len(line) for line in str(s).split('\n'))
 
@@ -28,47 +32,77 @@ def autodetectWidth(data):
         for i in range(len(line)):
             widths[i] = max(widths[i], mystrlen(line[i]))
 
-    col = os.get_terminal_size().columns
+    col = os.get_terminal_size().columns -12
     mm = sum(widths)
-    widths = [int(x*1.0/mm * col)-2 for x in widths]
+    widths = [int(x * 1.0 / mm * col)  for x in widths]
     return widths
 
+def format_dict(stock):
+    # stock['Ticker'] = f"{On_Black}" + stock['Ticker'] + f"{Color_Off}"
+    if stock['Change'][0] == '+':
+        stock['Price'] = f"{Green}" + stock['Price'] + f"{Color_Off}"
+        stock['Change'] = f"{On_Green}" + stock['Change'] + f"{Color_Off}"
+    elif stock['Change'][0] == '-':
+        stock['Price'] = f"{Red}" + stock['Price'] + f"{Color_Off}"
+        stock['Change'] = f"{On_Red}" + stock['Change'] + f"{Color_Off}"
+
+    # if stock['PrePostChange'][0] == '+':
+    #     stock['PrePostPrice'] = f"{Red}" + stock['Price'] + f"{Color_Off}"
+    #     stock['PrePostChange'] = f"{On_Green}" + stock['PrePostChange'] + f"{Color_Off}"
+    # elif stock['PrePostChange'][0] == '-':
+    #     stock['PrePostPrice'] = f"{Red}" + stock['Price'] + f"{Color_Off}"
+    #     stock['PrePostChange'] = f"{On_Red}" + stock['PrePostChange'] + f"{Color_Off}"
+
+    return stock
 
 
 def form_str(stocks):
-
-    new_list = [['TICKER','PRICE','PREPOST_PRICE']]
-
+    new_list = [['TICKER', 'PRICE','CHANGE', 'PREPOST_PRICE','CHANGE', 'DAYRANGE']]
+    len_list = [['TICKER', 'PRICE','CHANGE', 'PREPOST_PRICE','CHANGE', 'DAYRANGE']]
     for stock in stocks:
-        new_list.append(list(stock.values()))
-        new_list.append(['','',''])
-
+        len_list.append(list(stock.values()))
+        # stock = format_dict(stock)
+        new_list.append([stock['Ticker'], stock['Price'], stock['Change'], stock['PrePostPrice'], stock['PrePostChange'], stock['DayRange']])
+        # new_list.append(['', '', '','','',''])
 
     table = texttable.Texttable()
 
-    table.set_cols_align(["l", "l", "l"])
-    table.set_cols_valign(["l", "l", "l"])
+    table.set_cols_align(["l", "l", "l","l","l","l"])
+    table.set_cols_valign(["l", "l", "l","l","l","l"])
     table.set_deco(texttable.Texttable.HEADER)
-    table.set_header_align(["l", "l", "l"])
+    table.set_header_align(["l", "l", "l","l","l","l"])
 
-    table.header(['Ticker','Price','PrePostPrice'])
-
-
+    # table.header(['Ticker', 'Price', 'Change','PrePostPrice', 'Price ','Range'])
     table.set_cols_width(autodetectWidth(new_list))
-    table.add_rows(new_list)
+    # table.set_cols_width([20,20,20,20,20,20])
 
+    table.add_rows(new_list)
+    # import pdb; pdb.set_trace()
     return table.draw()
+
 
 while True:
     data_list = []
-    LIST_OF_STOCKS = ['TIGR','AAPL','BB','CRSR','FLGT','C6L.SI','F83.SI']
-    for stock in LIST_OF_STOCKS:
-        yp = YahooInfoGet(stock)
-        ans = yp.get_prices()
-        data_list.append(ans)
+    LIST_OF_STOCKS = ['TIGR', 'AAPL', 'BB', 'CRSR', 'FLGT','SPY', 'C6L.SI', 'F83.SI']
+    Test = False
+    if Test:
+        data_list = [{'Ticker': 'TIGR', 'Price': '26.62', 'PrePostPrice': '26.94', 'Change': '+2.54 (+10.55%)',
+                      'PrePostChange': '+0.32 (1.20%)', 'DayRange': '24.23 - 26.84'},
+                     {'Ticker': 'AAPL', 'Price': '133.70', 'PrePostPrice': '134.22', 'Change': '-0.28 (-0.21%)',
+                      'PrePostChange': '+0.52 (0.39%)', 'DayRange': '133.23 - 134.32'},
+                     {'Ticker': 'BB', 'Price': '13.14', 'PrePostPrice': '13.12', 'Change': '-0.27 (-2.01%)',
+                      'PrePostChange': '-0.02 (-0.15%)', 'DayRange': '12.91 - 13.47'},
+                     {'Ticker': 'CRSR', 'Price': '32.52', 'PrePostPrice': '32.63', 'Change': '+0.73 (+2.30%)',
+                      'PrePostChange': '+0.11 (0.34%)', 'DayRange': '31.89 - 32.96'}]
+
+    else:
+        for stock in LIST_OF_STOCKS:
+            yp = YahooInfoGet(stock)
+            ans = yp.get_result()
+            data_list.append(ans)
 
     now = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
-    date_str = "Updated Date: {}".format(now)
+    date_str = f"{Cyan}Updated Date: {now}{Color_Off}"
     col = os.get_terminal_size().columns
 
     right_side = col - len(date_str) - 10
@@ -79,14 +113,15 @@ while True:
     var['date_str'] = date_str
     var['line_break'] = line_break
 
-    res_string = """\r
- WFSTOCK%(empty_space)s%(date_str)s
+    res_string = f"""\r
+ {Yellow}WFSTOCK{Color_Off}%(empty_space)s%(date_str)s
 %(line_break)s
-    """ % var
-
+""" % var
 
     clear_console()
-    res_string  = res_string + form_str(data_list) + "\n\n"
-    print(res_string, end = '\r')
+    # res_string = ''
+    res_string = res_string + form_str(data_list) + "\n\n"
+    res_string = res_string.replace('\033[0m','\033[0m          ')
+    print(res_string, end='\r')
 
-    time.sleep(60*5)
+    time.sleep(60 * 5)
--- yahoo_scrap.py
+++ yahoo_scrap.py
@@ -10,6 +10,10 @@ class YahooInfoGet:
         self.soup = BeautifulSoup(page.content, 'html.parser')
         self.price = None
         self.pre_post_price = None
+        self.change = None
+        self.pre_post_change = None
+        self.pre_post_price = None
+        self.range = None
 
     def get_yahoo_price(self, key):
         price = self.soup.find_all("span", {"class": key})
@@ -19,21 +23,50 @@ class YahooInfoGet:
             price = 'NA'
         return price
 
+
+
+    def get_yahoo_change(self, key):
+        price = self.soup.find_all("span", {"class": [key + " C($positiveColor)", key + " C($negativeColor)"]})
+        try:
+            price = price[0].contents[0]
+        except BaseException:
+            price = 'NA'
+        return price
+
+    def get_result(self):
+        self.get_prices()
+        self.get_change()
+        self.get_range()
+        return {'Ticker': self.ticker, 'Price': self.price, 'PrePostPrice': self.pre_post_price, 'Change': self.change,
+                'PrePostChange': self.pre_post_change, 'DayRange': self.range
+                }
+
     def get_prices(self):
         self.price = self.get_yahoo_price("Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)")
         self.pre_post_price = self.get_yahoo_price("C($primaryColor) Fz(24px) Fw(b)")
-        return {'Ticker': self.ticker, 'Price': self.price, 'PrePostPrice': self.pre_post_price}
+
+    def get_change(self):
+        self.change = self.get_yahoo_change("Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px)")
+        self.pre_post_change = self.get_yahoo_change("Trsdu(0.3s) Mstart(4px) D(ib) Fz(24px)")
+
+    def get_range(self):
 
 
+
+        range = self.soup.findAll(attrs={"data-test" : "DAYS_RANGE-value"})
+        try:
+            range = range[0].contents[0]
+        except BaseException:
+            range = 'NA'
+
+        self.range = range
 
 
 if __name__ == '__main__':
     data_list = []
-    LIST_OF_STOCKS = ['TIGR','AAPL','BB','CRSR']
+    LIST_OF_STOCKS = ['TIGR', 'AAPL', 'BB', 'CRSR']
     for stock in LIST_OF_STOCKS:
         yp = YahooInfoGet(stock)
-        ans = yp.get_prices()
+        ans = yp.get_result()
         data_list.append(ans)
     print(data_list)
-
-
