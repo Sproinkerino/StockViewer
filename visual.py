3cc5a27d2fbe85e8cb37b2cded80847fb7379903
Sproinkerino <nwfxyz@gmail.com>
2021-06-28 16:46:58 +0800

    V2

--- visual.py
+++ visual.py
@@ -33,7 +33,7 @@ def autodetectWidth(data):
             widths[i] = max(widths[i], mystrlen(line[i]))
 
     col = os.get_terminal_size().columns -12
-    widths = [4,5,11,5,11,10]
+    widths = [4,5,10,5,10,9]
     mm = sum(widths)
     widths = [int(x * 1.0 / mm * col)  for x in widths]
     return widths
